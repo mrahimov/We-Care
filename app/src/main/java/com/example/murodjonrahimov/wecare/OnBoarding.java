cbec7af3c3a2b4e61c409933db8becbe57c034a1
Amar Ahmed <amar11432@gmail.com>
2018-03-26 03:56:51 -0400

    created initial onBoarding experience

---
+++ app/src/main/java/com/example/murodjonrahimov/wecare/OnBoarding.java
@@ -0,0 +1,144 @@
+package com.example.murodjonrahimov.wecare;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Amar on 3/26/18.
+ */
+public class OnBoarding extends AppCompatActivity{
+
+    private ViewPager viewPager;
+    private LinearLayout layoutDot;
+    private TextView[]dotstv;
+    private int[]layouts;
+    private Button btnSkip;
+    private Button btnNext;
+    private MyPagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        super.onCreate(savedInstanceState);
+
+        if(!isFirstTimeStartApp()) {
+            startMainActivity();
+            finish();
+        }
+
+        setStatusBarTransparent();
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_onboarding);
+
+        viewPager = findViewById(R.id.view_pager);
+        layoutDot = findViewById(R.id.dotLayout);
+        btnNext = findViewById(R.id.btn_next);
+        btnSkip = findViewById(R.id.btn_skip);
+
+        //When user press skip, start Main Activity
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startMainActivity();
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentPage = viewPager.getCurrentItem()+1;
+                if(currentPage < layouts.length) {
+                    //move to next page
+                    viewPager.setCurrentItem(currentPage);
+                } else {
+                    startMainActivity();
+                }
+            }
+        });
+        layouts = new int[]{R.layout.slider_1,R.layout.slider_2, R.layout.slider_3, R.layout.slider_4};
+        pagerAdapter = new MyPagerAdapter(layouts,getApplicationContext());
+        viewPager.setAdapter(pagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == layouts.length-1){
+                    //LAST PAGE
+                    btnNext.setText("START");
+                    btnSkip.setVisibility(View.GONE);
+                }else {
+                    btnNext.setText("NEXT");
+                    btnSkip.setVisibility(View.VISIBLE);
+                }
+                setDotStatus(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        setDotStatus(0);
+    }
+
+    private boolean isFirstTimeStartApp() {
+        SharedPreferences ref = getApplicationContext().getSharedPreferences("IntroSliderApp", Context.MODE_PRIVATE);
+        return ref.getBoolean("FirstTimeStartFlag", true);
+    }
+
+    private void setFirstTimeStartStatus(boolean stt) {
+        SharedPreferences ref = getApplicationContext().getSharedPreferences("IntroSliderApp", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = ref.edit();
+        editor.putBoolean("FirstTimeStartFlag", stt);
+        editor.commit();
+    }
+
+    private void setDotStatus(int page){
+        layoutDot.removeAllViews();
+        dotstv =new TextView[layouts.length];
+        for (int i = 0; i < dotstv.length; i++) {
+            dotstv[i] = new TextView(this);
+            dotstv[i].setText(Html.fromHtml("&#8226;"));
+            dotstv[i].setTextSize(30);
+            dotstv[i].setTextColor(Color.parseColor("#a9b4bb"));
+            layoutDot.addView(dotstv[i]);
+        }
+        //Set current dot active
+        if(dotstv.length>0){
+            dotstv[page].setTextColor(Color.parseColor("#ffffff"));
+        }
+    }
+    private void startMainActivity(){
+        setFirstTimeStartStatus(true);
+        startActivity(new Intent(OnBoarding.this, LoginActivity.class));
+        finish();
+    }
+    private void setStatusBarTransparent(){
+        if (Build.VERSION.SDK_INT >= 21){
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
+    }
+}
