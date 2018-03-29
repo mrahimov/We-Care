d131978d7110d81524c2ff40fc714609214036b4
Mohammad Naz <Mohammadnaz@ac.c4q.nyc>
2018-03-29 00:22:26 -0400

    doctorwithnamesForCommentsAndPost

--- app/src/main/java/com/example/murodjonrahimov/wecare/PostDoctorComments.java
+++ app/src/main/java/com/example/murodjonrahimov/wecare/PostDoctorComments.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import com.example.murodjonrahimov.wecare.controller.CommentsAdapter;
 import com.example.murodjonrahimov.wecare.database.Database;
 import com.example.murodjonrahimov.wecare.model.Comment;
@@ -27,128 +28,116 @@ import java.util.List;
 
 public class PostDoctorComments extends AppCompatActivity {
 
-  private String doctorMessage;
-  private String doctorTimeStamp;
-  private String name;
-  private EditText addedComment;
-  private List<Comment> allComments;
+    private String doctorMessage;
+    private String doctorTimeStamp;
+    private String name;
+    private EditText addedComment;
+    private List<Comment> allComments;
+    private DatabaseReference database10;
 
-  String commentname;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_post_doctor_comments);
-
-
-    TextView message = findViewById(R.id.message_d);
-    TextView addedBy = findViewById(R.id.added_by_d);
-    TextView timestamp = findViewById(R.id.timestamp_d);
-
-    //        why do we need to pass empty data here????/
-    Intent intent = getIntent();
-    final String Key = intent.getStringExtra("key");
-    doctorTimeStamp = intent.getStringExtra("timestamp");
-    name = intent.getStringExtra("addedby");
-    doctorMessage = intent.getStringExtra("message");
-
-    ImageView sendComment = findViewById(R.id.send_imageview);
-
-    addedComment = findViewById(R.id.addingcomment);
-    message.setText(doctorMessage);
-    addedBy.setText(name);
-    timestamp.setText(doctorTimeStamp);
-
-    RecyclerView recyclerView = findViewById(R.id.commentsrecyclerview);
-    final CommentsAdapter commentsAdapter = new CommentsAdapter();
-    LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-    recyclerView.setAdapter(commentsAdapter);
-    recyclerView.setLayoutManager(linearLayoutManager);
-      DatabaseReference database10;
-      String user = Database.getUserId();
-      database10 = FirebaseDatabase.getInstance().getReference().child("Doctor").child(user);
-
-
-
-      database10.getRef().addValueEventListener(new ValueEventListener() {
-          @Override
-          public void onDataChange(DataSnapshot dataSnapshot) {
-              if (dataSnapshot.exists()) {
-                  Doctor doctor= dataSnapshot.getValue(Doctor.class);
-                  commentname= doctor.getFirstName()+" "+doctor.getLastName();
-
-
-              } else {
-                  commentname = "doctor";
-              }
-              ///commentname= dataSnapshot.getValue(String.class);
-          }
-
-          @Override
-          public void onCancelled(DatabaseError databaseError) {
-
-          }
-      });
-
-
-    DatabaseReference db = Database.getDatabase();
-
-    db.child("comments")
-      .addValueEventListener(new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-
-          allComments = new ArrayList<>();
-
-          for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
-            Object postIdWrapper = dataSnapshot1.child("postId")
-              .getValue();
-            if (postIdWrapper != null) {
-              String retrievedPostId = postIdWrapper.toString();
-              if (retrievedPostId.equals(Key)) {
-                Comment comment = dataSnapshot1.getValue(Comment.class);
-                allComments.add(comment);
-              }
-            }
-          }
-          commentsAdapter.setData(allComments);
-          commentsAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-
-        }
-      });
-
-    sendComment.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-
-        String receivedComment = addedComment.getText()
-          .toString();
-
-
-
-        long date = System.currentTimeMillis();
-        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy h:mm a");
-        String dateString = sdf.format(date);
-
-        Comment comment = new Comment(receivedComment, Key, dateString, commentname);
-        Database.saveComment(comment);
-        //                int count = post.getCountOfComments() + 1;
-        //                post.setCountOfComments(count);
-        //                Database.updatePost(post.getKey(), count);
-        //
-        addedComment.getText()
-          .clear();
-      }
-    });
-  }
+    private String commentname;
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_doctor_comments);
 
+
+        TextView message = findViewById(R.id.message_d);
+        TextView addedBy = findViewById(R.id.added_by_d);
+        TextView timestamp = findViewById(R.id.timestamp_d);
+
+        Intent intent = getIntent();
+        final String Key = intent.getStringExtra("key");
+        doctorTimeStamp = intent.getStringExtra("timestamp");
+        name = intent.getStringExtra("addedby");
+        doctorMessage = intent.getStringExtra("message");
+
+        ImageView sendComment = findViewById(R.id.send_imageview);
+
+        addedComment = findViewById(R.id.addingcomment);
+        message.setText(doctorMessage);
+        addedBy.setText(name);
+        timestamp.setText(doctorTimeStamp);
+
+        RecyclerView recyclerView = findViewById(R.id.commentsrecyclerview);
+        final CommentsAdapter commentsAdapter = new CommentsAdapter();
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setAdapter(commentsAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        String user = Database.getUserId();
+        database10 = FirebaseDatabase.getInstance().getReference().child("doctors").child(user);
+
+
+        database10.getRef().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Doctor doctor = dataSnapshot.getValue(Doctor.class);
+                    commentname = doctor.getFirstName() + " " + doctor.getLastName();
+                } else {
+                    commentname = "doctor";
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        DatabaseReference db = Database.getDatabase();
+
+        db.child("comments")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+
+                        allComments = new ArrayList<>();
+
+                        for (DataSnapshot dataSnapshot1 : dataSnapshot.getChildren()) {
+                            Object postIdWrapper = dataSnapshot1.child("postId")
+                                    .getValue();
+                            if (postIdWrapper != null) {
+                                String retrievedPostId = postIdWrapper.toString();
+                                if (retrievedPostId.equals(Key)) {
+                                    Comment comment = dataSnapshot1.getValue(Comment.class);
+                                    allComments.add(comment);
+                                }
+                            }
+                        }
+                        commentsAdapter.setData(allComments);
+                        commentsAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+        sendComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String receivedComment = addedComment.getText()
+                        .toString();
+                long date = System.currentTimeMillis();
+                SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy h:mm a");
+                String dateString = sdf.format(date);
+
+                Comment comment = new Comment(receivedComment, Key, dateString, commentname);
+                Database.saveComment(comment);
+                //                int count = post.getCountOfComments() + 1;
+                //                post.setCountOfComments(count);
+                //                Database.updatePost(post.getKey(), count);
+                //
+                addedComment.getText()
+                        .clear();
+            }
+        });
     }
+
 }
